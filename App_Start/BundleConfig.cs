0017e3efbb8e60ddb381905f815c49cc2d272ba0
Shahd El-Badrawy <shahdbadrawy@gmail.com>
2023-07-12 04:13:15 +0300

    3rd commit

--- App_Start/BundleConfig.cs
+++ App_Start/BundleConfig.cs
@@ -20,13 +20,12 @@ namespace Books
 						"~/Scripts/modernizr-*"));
 
 			bundles.Add(new Bundle("~/bundles/bootstrap").Include(
-					  "~/Scripts/bootstrap.js", 
+					  "~/Scripts/bootstrap.js",
 					  "~/Scripts/toaster.min.js"));
 
 			bundles.Add(new StyleBundle("~/Content/css").Include(
 					  "~/Content/bootstrap.css",
-					  "~/Content/icofont.min.css",
-					  "~/Cotent/toaster.min.css",
+					  "~/Content/toaster.min.css",
 					  "~/Content/site.css"));
 		}
 	}
